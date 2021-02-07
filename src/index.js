15760d6ee780b860d1bb7530db5a1ecca6ba69e2
Gabriela Johnson <johnsongabriela12@gmail.com>
2021-02-06 17:57:17 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DisplayData from './components/DisplayData';
 import Form from './components/Form'
+import ParentComponent from './components/ParentComponent'
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
+    {/* <DisplayData /> */}
+   
   </div>,
   document.getElementById('root')
 );
